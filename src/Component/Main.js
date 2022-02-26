c76700a6337a6ddf3c5e65144a521c5105217a7a
chanksela <kakhachankseliani@gmail.com>
2022-02-26 10:01:51 +0400

    trying fetch

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,12 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MainCSS from "./Main.module.css";
 
 export const Main = () => {
+  const [input, setInput] = useState("");
+  const [name, setName] = useState("");
+  const [link, setLink] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const handleInput = (e) => {
+    setInput(e.target.value);
+    console.log(input);
+  };
+  // light/dark theme handler
   const [lightTheme, setLightTheme] = useState(false);
   const handleLightTheme = () => {
     setLightTheme(!lightTheme);
-    console.log(lightTheme);
   };
+  // test api
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/example")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
+  const setData = ({ login, html_url, avatar_url }) => {
+    setName(login);
+    setLink(html_url);
+    setAvatar(avatar_url);
+  };
+
   return (
     <div className={lightTheme ? MainCSS.dark : MainCSS.light}>
       <button
@@ -16,8 +40,17 @@ export const Main = () => {
         {lightTheme ? "Light" : "Dark"}
       </button>
       <div id={lightTheme ? MainCSS.search : MainCSS.searchLight}>
-        <input type="search" id="search-user" />
+        <form>
+          {" "}
+          <input type="search" id="search-user" onChange={handleInput} />
+        </form>
       </div>
+      <h4>
+        <a target="_blank" href={link}>
+          <img src={avatar} />
+          <h5>{name}</h5>
+        </a>
+      </h4>
       <h5 id={MainCSS.footer}>
         Created by{" "}
         <a
