c4f66c00dfcf1cef5664fd305c54ee0bd9d277bd
chanksela <kakhachankseliani@gmail.com>
2022-03-01 08:42:44 +0400

    token

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import MainCSS from "./Main.module.css";
 
 export const Main = () => {
-  const key = process.env.KEY;
   // light/dark theme handler
   const [lightTheme, setLightTheme] = useState(false);
   const [userName, setUserName] = useState();
@@ -15,17 +14,23 @@ export const Main = () => {
 
   const [wordEntered, setWordEntered] = useState("");
   let array = [];
+
   async function handleFilter(event) {
     const searchWord = event.target.value;
     setWordEntered(searchWord);
     const baseURL = `https://api.github.com/users/${wordEntered}`;
-    const response = await fetch(baseURL, key);
+    const response = await fetch(baseURL, {
+      method: "GET",
+      headers: {
+        Authorization: process.env.TOKEN,
+      },
+    });
     const data = await response.json();
     array.push(data);
     setUserName(array[0].login);
     setProfile(array[0].html_url);
     setAvatar(array[0].avatar_url);
-    console.log(array);
+    console.log(wordEntered);
   }
 
   return (
@@ -36,15 +41,25 @@ export const Main = () => {
       ></div>
 
       <div id={lightTheme ? MainCSS.search : MainCSS.searchLight}>
-        <input type="text" value={wordEntered} onChange={handleFilter} />
+        <input
+          type="text"
+          value={wordEntered}
+          onChange={handleFilter}
+          placeholder="type here..."
+        />
         {wordEntered.length > 0 && (
           <div
             className={
               !lightTheme ? MainCSS.typeaheadLight : MainCSS.typeaheadDark
             }
           >
-            <a classname={MainCSS.userInfo} href={profile} target="_blank">
-              <img className={MainCSS.avatar} src={avatar} />
+            <a
+              className={MainCSS.userInfo}
+              href={profile}
+              rel="noreferrer"
+              target="_blank"
+            >
+              <img className={MainCSS.avatar} src={avatar} alt="avatar" />
               <p className={MainCSS.userName}>{userName}</p>
             </a>
           </div>
@@ -56,6 +71,7 @@ export const Main = () => {
         <a
           href="https://github.com/Chanksela"
           target="_blank"
+          rel="noreferrer"
           id={lightTheme ? MainCSS.author : MainCSS.authorLight}
         >
           {" "}
